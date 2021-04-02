7ef9cf69a97f309c849ac102f883738e7c3cbcb3
Alex <alexlawn333@gmail.com>
2021-04-02 19:04:03 +0100

    Make addresses clickable and have them open the address in Google Maps

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import './Business.css';
 
+
 class Business extends React.Component{
+    constructor(props) {
+        super(props);
+        this.googleMaps = this.googleMaps.bind(this);
+    }
+
+    googleMaps(event) {
+        const address =
+        `https://www.google.co.uk/maps/place/${this.props.business.address},+${this.props.business.city}`;
+        window.location.href = address;
+    }
+    
     render(){
         return (
         <div className="Business">
@@ -10,7 +22,7 @@ class Business extends React.Component{
             </div>
             <h2>{this.props.business.name}</h2>
             <div className="Business-information">
-                <div className="Business-address">
+                <div className="Business-address" onClick={this.googleMaps}>
                     <p>{this.props.business.address}</p>
                     <p>{this.props.business.city}</p>
                     <p>{this.props.business.state} {this.props.business.zipCode}</p>
